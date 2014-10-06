59dbd28c7ceab13f34aa3e5074b2e3777936377a
Sahil Dhankhar <sahil1345@gmail.com>
2014-10-06 21:57:26 +0530

    Next Up email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ActionMailer::Base
-  default from: "YesAndApp@gmail.com"
+  default from: '"Sil Demmer"<YesAndApp@gmail.com>'
   
   def welcome
     mail(to: "sil.demmer@gmail.com", subject: "Trial") do |format|
       format.html { render :text => "Lets do it"}
     end
+  end
+  
+  def notify_of_turn(nextup, sentence)
+    @nextup = nextup
+    @sentence = sentence
+    mail(to: nextup.email, subject: "Now its your turn to keep the ball rolling!")
   end
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -10,4 +10,12 @@ class Sentence < ActiveRecord::Base
   }
   belongs_to :user
   belongs_to :group
+  after_create :send_email_to_nextup_user
+  
+  def send_email_to_nextup_user
+    nextup = self.group.users_order.first
+    unless nextup.id != self.user_id
+      UserMailer.notify_of_turn(nextup, self).deliver
+    end 
+  end
 end
