8626deb499ef80b7c80aff09080adf0a41023bf7
Sahil Dhankhar <sahil1345@gmail.com>
2014-10-06 19:53:18 +0530

    for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,50 @@
+require 'date'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  TEMP_EMAIL_PREFIX = 'change@me'
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :omniauthable
   validates_uniqueness_of :username
   validates_presence_of :firstname, :lastname, :username
   has_many :user_groups, :dependent => :destroy
   has_many :groups, through: :user_groups
   has_many :sentences
+  has_many :identities, :dependent => :destroy
+  
+  def self.find_for_oauth(auth, signed_in_resource = nil)
+    identity = Identity.find_for_oauth(auth)
+    user = signed_in_resource ? signed_in_resource : identity.user
+    if user.nil?
+      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+      email = auth.info.email if email_is_verified
+      user = User.where(:email => email).first if email
+      if user.nil?
+          user = self.generate_user_from_facebook(auth, email)  
+        user.save!
+      end
+    end
+    if identity.user != user
+      identity.user = user
+      identity.save!
+    end
+    user
+  end
+  
+  def self.generate_user_from_facebook(auth, email)
+    puts auth.info.name
+      user = User.new(firstname: auth.info.first_name, lastname: auth.info.last_name, username: generate_unique_username(auth.info.name),
+        email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+        password: Devise.friendly_token[0,20],
+      )
+  end
+  
+  private # all methods that follow will be made private: not accessible for outside objects
+  def self.generate_unique_username(name)
+    username = name.gsub(/\s+/, "")
+    timestamp = DateTime.now.strftime('%Q').to_s
+    timestamp_len = timestamp.length
+    username = username + timestamp[timestamp_len-4..timestamp_len]
+    username
+  end
 end
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.gmail.com',
   :port => 587,
   :domain => "localhost:3000",
-  :user_name => Figaro.env.email_username,
-  :password => Figaro.env.email_secret,
+  :user_name => ENV['email_username'],
+  :password => ENV['email_secret'],
   :authentication => "plain",
   :enable_starttls_auto => true
 }
