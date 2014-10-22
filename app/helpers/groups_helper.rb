3ebbd9aa2b7c24a16caebb238bfaf9299ea7e883
Sahil Dhankhar <sahil1345@gmail.com>
2014-10-22 21:58:17 +0530

    Fixes

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,12 +2,17 @@ module GroupsHelper
 
   def twitter_text(group)
     url = result_group_url(group)
-    result = @group.sentences.map(&:content).join('. ')
+    result = group.sentences.map(&:content).join('. ')
     final_len = 140 - url.length
     truncate(result,:length => final_len)  
   end
   
   def groups_titles()
     group_titles_combo = Group.all.map(&:title).join(';')
+  end
+  
+  def fb_text(group)
+    result = group.sentences.map(&:content).join('. ')
+    truncate(result,:length => 900)
   end
 end
