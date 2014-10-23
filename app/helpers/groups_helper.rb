4bc5366ce3ed00b0f9bb96e6cd7921c6366fb688
Sahil Dhankhar <sahil1345@gmail.com>
2014-10-23 14:34:51 +0530

    for google plus

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -7,13 +7,6 @@ module GroupsHelper
     truncate(result,:length => final_len)  
   end
   
-  def fb_text(group)
-    url = result_group_url(group)
-    result = @group.sentences.map(&:content).join('. ')
-    final_len = 900 - url.length
-    truncate(result,:length => final_len)  
-  end
-  
   def groups_titles()
     group_titles_combo = Group.all.map(&:title).join(';')
   end
