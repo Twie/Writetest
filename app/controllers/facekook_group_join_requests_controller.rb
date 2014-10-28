b2fcf6bf30cb81649773fd2724528757c28f8539
Sahil Dhankhar <sahil1345@gmail.com>
2014-10-28 22:26:54 +0530

    Fixes

--- app/controllers/facekook_group_join_requests_controller.rb
+++ app/controllers/facekook_group_join_requests_controller.rb
@@ -1,7 +1,7 @@
 class FacekookGroupJoinRequestsController < ApplicationController
   def create
     @facebook_group_join_request = FacekookGroupJoinRequest.new(facebook_group_join_request_params)
-    if @facebook_group_join_request.save
+    if @facebook_group_join_request.save!
       render :json => true
     else
       render :json => false
